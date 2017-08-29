015f3210b388cadaa78807018435b34a2fe5913f
ghost1372 <mahdidvb72@gmail.com>
2017-08-29 14:14:09 +0430

    Create project in Studio. for Easy Work with Archives

---
+++ m-zip/src/main/java/ir/mahdi/mzip/zip/ZipArchive.java
@@ -0,0 +1,51 @@
+package ir.mahdi.mzip.zip;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+
+import java.io.File;
+
+/**
+ * Created by Mahdi on 8/29/2017.
+ */
+
+public class ZipArchive {
+    public static void zip(String targetPath, String destinationFilePath, String password) {
+        try {
+            ZipParameters parameters = new ZipParameters();
+            parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+            parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+
+            if (password.length() > 0) {
+                parameters.setEncryptFiles(true);
+                parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
+                parameters.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+                parameters.setPassword(password);
+            }
+
+            ZipFile zipFile = new ZipFile(destinationFilePath);
+
+            File targetFile = new File(targetPath);
+            if (targetFile.isFile()) {
+                zipFile.addFile(targetFile, parameters);
+            } else if (targetFile.isDirectory()) {
+                zipFile.addFolder(targetFile, parameters);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void unzip(String targetZipFilePath, String destinationFolderPath, String password) {
+        try {
+            ZipFile zipFile = new ZipFile(targetZipFilePath);
+            if (zipFile.isEncrypted()) {
+                zipFile.setPassword(password);
+            }
+            zipFile.extractAll(destinationFolderPath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
